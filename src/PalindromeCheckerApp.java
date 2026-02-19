ee2674f854f0bab8d26600201ecc15f19ac0e893
Ashraf <mohdashrafkkhan@email.com>
2026-02-19 09:34:12 +0530

    UC3:Check for palindrom by Reversing the string using for loop

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,24 +1,17 @@
 import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main (String[] args){
-        System.out.println("Welcome to the Palindrome checker APP");
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a string");
         String s=sc.nextLine();
-        s = s.toLowerCase().replaceAll("\\s", "");
-        int l=0;
-        int h=s.length()-1;
-        int flag=0;
-        while(l<h){
-            if(s.charAt(l)!=s.charAt(h)){
-                System.out.println("Its not a palindrome");
-                flag=1;
-                break;}
-            l++;
-            h--;
-        }if(flag==0){
-            System.out.println("It is a palindrome");
+        String s2="";
+        for(int i=s.length()-1;i>=0;i--){
+            s2=s2+s.charAt(i);
         }
-
+        if (s.equals(s2)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
     }
 }
